09cf71f42f85a1642c2d3df388ffc57a296643ac
Miki@0929054164 <milkiyasgebru@gmail.com>
2025-03-31 20:33:20 +0300

    Added Features section

---
+++ components/Features.jsx
@@ -0,0 +1,58 @@
+import {BiMessage, BiPhotoAlbum, BiCommand} from "react-icons/bi";
+
+
+export default function Features() {
+
+    return (
+        <div className="flex flex-col gap-4 mt-8 py-4">
+            <p>[ Capabilities ]</p>
+            <h2>Model that fits your needs</h2>
+            <div className="flex flex-wrap">
+                {/*Features*/}
+                <div className="flex flex-col gap-3 w-1/3 p-4 border-l border-gray-600 ">
+                    <BiMessage className="text-4xl" />
+                    <h3 className="text-2xl">Text</h3>
+                    <p className="text-gray-400">The cornerstone of interaction; summarize articles, generate
+                        creative writing, assist with coding tasks and more
+                    </p>
+                </div>
+                <div className="flex flex-col gap-3 w-1/3 p-4 border-l border-gray-600">
+                    <BiPhotoAlbum className="text-4xl" />
+                    <h3>Text</h3>
+                    <p>The cornerstone of interaction; summarize articles, generate
+                        creative writing, assist with coding tasks and more
+                    </p>
+                </div>
+                <div className="flex flex-col gap-3 w-1/3 p-4 border-l border-gray-600">
+                    <BiCommand className="text-4xl" />
+                    <h3>Text</h3>
+                    <p>The cornerstone of interaction; summarize articles, generate
+                        creative writing, assist with coding tasks and more
+                    </p>
+                </div>
+                <div className="flex flex-col gap-3 w-1/3 p-4 border-l border-gray-600">
+                    <BiMessage className="text-4xl" />
+                    <h3>Text</h3>
+                    <p>The cornerstone of interaction; summarize articles, generate
+                        creative writing, assist with coding tasks and more
+                    </p>
+                </div>
+                <div className="flex flex-col gap-3 w-1/3 p-4 border-l border-gray-600">
+                    <BiMessage className="text-4xl" />
+                    <h3>Text</h3>
+                    <p>The cornerstone of interaction; summarize articles, generate
+                        creative writing, assist with coding tasks and more
+                    </p>
+                </div>
+                <div className="flex flex-col gap-3 w-1/3 p-4 border-l border-gray-600">
+                    <BiMessage className="text-4xl" />
+                    <h3>Text</h3>
+                    <p>The cornerstone of interaction; summarize articles, generate
+                        creative writing, assist with coding tasks and more
+                    </p>
+                </div>
+
+            </div>
+        </div>
+    )
+}
