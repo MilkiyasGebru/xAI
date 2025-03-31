d2c2fac1ab7dc904b5e8ae3a593375cdbe68c2cc
Miki@0929054164 <milkiyasgebru@gmail.com>
2025-03-31 20:43:40 +0300

    Added the Quick start section

---
+++ components/QuickStart.jsx
@@ -0,0 +1,13 @@
+export default function QuickStart() {
+    return (
+        <div className="flex flex-col w-1/2">
+            <div className="flex flex-col gap-4">
+                <p className="capitalize">[ minutes to launch ]</p>
+                <h2 className="text-3xl">Quickstart</h2>
+                <p>Our API is compatible with OpenAI and Anthropic’s SDKs.</p>
+                <p>Migrating is as easy as generating an API key and changing a URL.</p>
+                <button className="capitalize px-4 py-2 rounded-full w-fit border">START BUILDING</button>
+            </div>
+        </div>
+    )
+}
