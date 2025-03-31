e349d0a654bf42d0c3b8fd9caf2a52538fdc9adb
Miki@0929054164 <milkiyasgebru@gmail.com>
2025-03-31 21:31:49 +0300

    Added the Coming Soon Section

---
+++ components/ComingSoon.jsx
@@ -0,0 +1,11 @@
+export default function ComingSoon() {
+
+    return (
+        <div className="flex flex-col justify-center items-center">
+            <h3>Grok 3 coming soon</h3>
+            <p>We're working hard to bring Grok 3 to the API. We'll be rolling out access soon.</p>
+            <button className="px-3 py-2 border rounded-full">Read More</button>
+        </div>
+    )
+
+}
