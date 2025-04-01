7aafa474201b9d092edcff972dbf4079f4517571
Miki@0929054164 <milkiyasgebru@gmail.com>
2025-04-01 10:22:51 +0300

    Changed the down arrow icon to a better one

--- components/ComingSoon.jsx
+++ components/ComingSoon.jsx
@@ -1,10 +1,12 @@
+import { HiArrowUpRight } from 'react-icons/hi2';
+
 export default function ComingSoon() {
 
     return (
-        <div className="flex flex-col justify-center items-center">
-            <h3>Grok 3 coming soon</h3>
-            <p>We're working hard to bring Grok 3 to the API. We'll be rolling out access soon.</p>
-            <button className="px-3 py-2 border rounded-full">Read More</button>
+        <div className="flex flex-col justify-center items-center py-12 gap-y-6">
+            <h3 className="text-2xl text-white">Grok 3 coming soon</h3>
+            <p className="text-gray-500">We're working hard to bring Grok 3 to the API. We'll be rolling out access soon.</p>
+            <button className="px-3 py-2 border rounded-full text-white flex items-center gap-2">READ MORE <HiArrowUpRight/> </button>
         </div>
     )
 
