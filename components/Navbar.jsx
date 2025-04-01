eb36a77962dc1791b1686dc6936758311136b53b
Miki@0929054164 <milkiyasgebru@gmail.com>
2025-04-01 09:42:33 +0300

    Added the proper margin

--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -3,13 +3,13 @@ const navbar_buttons = ["GROK", "API", "COMPANY", "COLOSSUS", "CAREERS", "NEWS"]
 export default function Navbar(){
 
     return (
-        <nav className="py-2 flex justify-between items-center">
+        <nav className="py-4 flex justify-between items-center sticky top-0 z-10 bg-black max-w-[1124px] mx-auto opacity-95">
             <div className="flex gap-x-3">
                 {navbar_buttons.map((btn, i) => (
-                    <button key={i} className="text-gray-300 hover:text-white cursor-pointer">{btn}</button>
+                    <button key={i} className="text-neutral-400 hover:text-white cursor-pointer">{btn}</button>
                 ))}
             </div>
-            <button className="px-2 py-1 rounded-full border text-white">
+            <button className="px-2 py-1 rounded-full border text-neutral-400 hover:text-white transition duration-500">
                 TRY GROK
             </button>
         </nav>
