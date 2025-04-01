34c3329c42c9bdc00c106a12f7d30c98ba64974b
Miki@0929054164 <milkiyasgebru@gmail.com>
2025-04-01 08:41:50 +0300

    Fixed the letter spacing and added down arrow

--- components/Intro.jsx
+++ components/Intro.jsx
@@ -2,16 +2,16 @@ import {BiDownArrowAlt} from "react-icons/bi";
 
 export default function Intro(){
     return (
-        <div className="min-h-[80vh] flex flex-col justify-between py-2 mt-6 border-b border-gray-200">
+        <div className="min-h-[90vh] flex flex-col justify-between py-12 mt-6 border-b border-gray-500">
             <div className="flex flex-col gap-y-12">
-                <p className="capitalize">[ Elevate your apps]</p>
+                <p className="tracking-widest text-md">[ ELEVATE YOUR APPS]</p>
                 <div className="flex flex-col text-7xl">
                     <p>The Next Frontier</p>
                     <p>in AI integration</p>
                 </div>
             </div>
             <div className="flex justify-between">
-                <BiDownArrowAlt className="text-4xl" />
+                <BiDownArrowAlt className="text-4xl font-thin" />
                 <div className="flex gap-x-3">
                     <button className="px-3 py-2 rounded-full border bg-white text-black">DOCUMENTATION</button>
                     <button className="px-3 py-2 rounded-full border">PRICING</button>
