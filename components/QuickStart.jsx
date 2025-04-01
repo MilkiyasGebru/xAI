c34cd410b139f829c523df341c19ac905893ebd8
Miki@0929054164 <milkiyasgebru@gmail.com>
2025-04-01 10:32:08 +0300

    Adjusted the gaps and fixed the font colors

--- components/QuickStart.jsx
+++ components/QuickStart.jsx
@@ -3,12 +3,12 @@ export default function QuickStart() {
 
         <section className="max-w-[1124px] mx-auto flex">
             <div className="flex flex-col w-1/2 mt-8 ">
-                <div className="flex flex-col gap-4">
+                <div className="flex flex-col ">
                     <p className="capitalize">[ minutes to launch ]</p>
-                    <h2 className="text-3xl font-semibold">Quickstart</h2>
-                    <p>Our API is compatible with OpenAI and Anthropic’s SDKs.</p>
-                    <p>Migrating is as easy as generating an API key and changing a URL.</p>
-                    <button className="capitalize px-4 py-2 rounded-full w-fit border">START BUILDING</button>
+                    <h2 className="text-5xl text-white font-semibold mt-12">Quickstart</h2>
+                    <p className="text-white mt-6">Our API is compatible with OpenAI and Anthropic’s SDKs.</p>
+                    <p className="text-white">Migrating is as easy as generating an API key and changing a URL.</p>
+                    <button className="capitalize px-4 py-2 rounded-full w-fit border mt-12 text-white">START BUILDING</button>
                 </div>
             </div>
         </section>
