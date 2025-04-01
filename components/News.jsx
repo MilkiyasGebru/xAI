c579e0660a7817546d6af7b13ab3edef9baf483f
Miki@0929054164 <milkiyasgebru@gmail.com>
2025-04-01 12:30:50 +0300

    Added the Latest News and View All Button to the News Page

--- components/News.jsx
+++ components/News.jsx
@@ -1,52 +1,68 @@
 export default function NewsPage() {
     return (
-        <div className="flex justify-between">
-            <div className="w-[30%] flex flex-col ">
-                <div
-                    className="bg-[url(https://x.ai/images/news/grok-3.webp)] w-full aspect-square flex justify-center items-center bg-cover">
-                    <span className="bg-black text-white text-4xl px-2 py-1">GROK 3</span>
+
+        <section className="mx-auto max-w-[1124px]">
+            <div className="flex flex-col">
+
+                <div className="flex justify-between items-center mb-12">
+                    <span className="text-4xl text-white">Latest News</span>
+                    <button className="px-4 py-2 rounded-full border border-gray-600 text-white tracking-wider">VIEW ALL</button>
                 </div>
-                <span className="text-lg font-semibold mt-2">Grok 3 Beta — The Age of Reasoning Agents</span>
-                <p className="text-md mt-2">
-                    We are thrilled to unveil an early preview of Grok 3,
-                    our most advanced model yet, blending superior reasoning with extensive pretraining knowledge.
-                </p>
-                <div className="flex justify-between mt-8 items-center">
-                    <span className="text-md">February 19, 2025</span>
-                    <button className="px-2 py-1 rounded-full border ">READ</button>
+
+                <div className="flex justify-between">
+                    <div className="w-[30%] flex flex-col ">
+                        <div
+                            className="bg-[url(https://x.ai/images/news/grok-3.webp)] w-full aspect-square flex justify-center items-center bg-cover">
+                            <span className="bg-black text-white text-4xl px-2 py-1">GROK 3</span>
+                        </div>
+                        <span className="text-lg font-semibold mt-2">Grok 3 Beta — The Age of Reasoning Agents</span>
+                        <p className="text-md mt-2">
+                            We are thrilled to unveil an early preview of Grok 3,
+                            our most advanced model yet, blending superior reasoning with extensive pretraining
+                            knowledge.
+                        </p>
+                        <div className="flex justify-between mt-8 items-center">
+                            <span className="text-md">February 19, 2025</span>
+                            <button className="px-2 py-1 rounded-full border ">READ</button>
+                        </div>
+                    </div>
+                    <div className="w-[30%] flex flex-col ">
+                        <div
+                            className="bg-[url(https://x.ai/images/news/grok-3.webp)] w-full aspect-square flex justify-center items-center bg-cover">
+                            <span className="bg-black text-white text-4xl px-2 py-1">GROK 3</span>
+                        </div>
+                        <span className="text-lg font-semibold mt-2">Grok 3 Beta — The Age of Reasoning Agents</span>
+                        <p className="text-md mt-2">
+                            We are thrilled to unveil an early preview of Grok 3,
+                            our most advanced model yet, blending superior reasoning with extensive pretraining
+                            knowledge.
+                        </p>
+                        <div className="flex justify-between mt-8 items-center">
+                            <span className="text-md">February 19, 2025</span>
+                            <button className="px-2 py-1 rounded-full border ">READ</button>
+                        </div>
+                    </div>
+                    <div className="w-[30%] flex flex-col ">
+                        <div
+                            className="bg-[url(https://x.ai/images/news/grok-3.webp)] w-full aspect-square flex justify-center items-center bg-cover">
+                            <span className="bg-black text-white text-4xl px-2 py-1">GROK 3</span>
+                        </div>
+                        <span className="text-lg font-semibold mt-2">Grok 3 Beta — The Age of Reasoning Agents</span>
+                        <p className="text-md mt-2">
+                            We are thrilled to unveil an early preview of Grok 3,
+                            our most advanced model yet, blending superior reasoning with extensive pretraining
+                            knowledge.
+                        </p>
+                        <div className="flex justify-between mt-8 items-center">
+                            <span className="text-md">February 19, 2025</span>
+                            <button className="px-2 py-1 rounded-full border ">READ</button>
+                        </div>
+                    </div>
                 </div>
             </div>
-            <div className="w-[30%] flex flex-col ">
-                <div
-                    className="bg-[url(https://x.ai/images/news/grok-3.webp)] w-full aspect-square flex justify-center items-center bg-cover">
-                    <span className="bg-black text-white text-4xl px-2 py-1">GROK 3</span>
-                </div>
-                <span className="text-lg font-semibold mt-2">Grok 3 Beta — The Age of Reasoning Agents</span>
-                <p className="text-md mt-2">
-                    We are thrilled to unveil an early preview of Grok 3,
-                    our most advanced model yet, blending superior reasoning with extensive pretraining knowledge.
-                </p>
-                <div className="flex justify-between mt-8 items-center">
-                    <span className="text-md">February 19, 2025</span>
-                    <button className="px-2 py-1 rounded-full border ">READ</button>
-                </div>
-            </div>
-            <div className="w-[30%] flex flex-col ">
-                <div
-                    className="bg-[url(https://x.ai/images/news/grok-3.webp)] w-full aspect-square flex justify-center items-center bg-cover">
-                    <span className="bg-black text-white text-4xl px-2 py-1">GROK 3</span>
-                </div>
-                <span className="text-lg font-semibold mt-2">Grok 3 Beta — The Age of Reasoning Agents</span>
-                <p className="text-md mt-2">
-                    We are thrilled to unveil an early preview of Grok 3,
-                    our most advanced model yet, blending superior reasoning with extensive pretraining knowledge.
-                </p>
-                <div className="flex justify-between mt-8 items-center">
-                    <span className="text-md">February 19, 2025</span>
-                    <button className="px-2 py-1 rounded-full border ">READ</button>
-                </div>
-            </div>
-        </div>
+        </section>
+
+
     )
 }
 
