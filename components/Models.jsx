c8d1c367e4df1eaf1a8535e3f291a9cf14e2431c
Miki@0929054164 <milkiyasgebru@gmail.com>
2025-04-01 09:58:44 +0300

    Changed the font size and text color

--- components/Models.jsx
+++ components/Models.jsx
@@ -3,7 +3,7 @@ export default function Models() {
 
         <section className="max-w-[1124px] mx-auto mt-40">
             <div className="flex flex-col py-6   ">
-                <p className="tracking-wider">[ FOR PROJECTS BIG AND SMALL ]</p>
+                <p className="tracking-wider space-x-1.5">[ FOR PROJECTS BIG AND SMALL ]</p>
                 <h2 className="text-5xl text-white font-semibold mt-16 mb-24">Models and Pricing</h2>
                 <div className="flex flex-col">
                     <div className="flex justify-between py-3 border-b border-gray-600">
@@ -17,11 +17,11 @@ export default function Models() {
                         </div>
                     </div>
                     <div className="flex justify-between py-3 border-b border-gray-600">
-                        <div className="flex flex-col">
-                            <span className="text-2xl font-semibold">grok-2-1212</span>
-                            <span>Grok 2 model supporting function calling and structured outputs.</span>
+                        <div className="flex flex-col w-1/2 gap-2">
+                            <span className="text-2xl font-semibold text-white">grok-2-1212</span>
+                            <span className="w-1/2 text-md tracking-tighter">Grok 2 model supporting function calling and structured outputs.</span>
                         </div>
-                        <div className="flex justify-between w-1/2 items-center text-xl font-semibold">
+                        <div className="flex justify-between w-1/2 items-center text-2xl font-semibold text-white">
                             <span>131,072</span>
                             <span>$2.00	</span>
                             <span>-</span>
@@ -30,11 +30,11 @@ export default function Models() {
                         </div>
                     </div>
                     <div className="flex justify-between py-3 border-b border-gray-600">
-                        <div className="flex flex-col w-1/4 flex-wrap">
-                            <span className="text-2xl font-semibold">grok-2-1212</span>
-                            <span>Grok 2 model supporting function  calling and structured outputs.</span>
+                        <div className="flex flex-col w-1/4 flex-wrap gap-2">
+                            <span className="text-2xl font-semibold text-white">grok-2-1212</span>
+                            <span className="tracking-tighter">Grok 2 model supporting function  calling and structured outputs.</span>
                         </div>
-                        <div className="flex justify-between w-1/2 items-center text-xl font-semibold">
+                        <div className="flex justify-between w-1/2 items-center text-2xl font-semibold text-white">
                             <span>32,768</span>
                             <span>$2.00	</span>
                             <span>$2.00	</span>
